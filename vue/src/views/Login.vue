45cb29b7161345c75fe21635336be096922d0cdd
JerrymihMercado <116856008+JerrymihMercado@users.noreply.github.com>
2023-03-04 10:14:23 +0800

    jerrymih- man pa test muna kung makakapag log in

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -2,49 +2,39 @@
  <div class="login-container">
   <div class="container">
     <form @submit="login">
-      <div class="row">
-         
-        <div class="col-md-5 pt-5 test">
-          <div class="card">
-            <div class="card-body form-body">
-              <div class="clsu-logo text-center">
-                <img
-                  class="img-logo"
-                  src="../assets/logos/clsu-logo.png"
-                  alt="logo"
-                />
-              </div>
-              <h3 class="text-center pb-2">Welcome Back</h3>
-
-              <div class="form pt-4">
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autocomplete="email"
-                  v-model="user.email"
-                  class="input"
-                  placeholder="Username"
-                  required=""
-                /><i class="bi bi-envelope-fill text-white text-center"></i>
-              </div>
-              <div class="form pt-4">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autocomplete="current-password"
-                  required=""
-                  v-model="user.password"
-                  class="input"
-                  placeholder="Password"
-                /><i class="bi bi-lock-fill text-white text-center"></i>
-              </div>
-
-              <a href="#" class="forgot"
-                ><p class="forgot-txt pt-2">Forgot password?</p></a
-              >
-              <div class="submit-container pb-4 pt-2">
+      <div class="row pt-4">
+        <div class="col-4  pt-5 card-base ">
+            <div class="logo text-center">
+              <img src="../assets/logos/logo-main.png" alt="" class="logo-main">
+            </div>
+                        
+            <h2 class="text-center welcome-txt">Welcome! </h2>
+                        
+                        <div class="form pt-4">
+                            <input
+                              id="email-address"
+                              name="email"
+                              type="email"
+                              autocomplete="email"
+                              v-model="user.email"
+                              class="input"
+                              placeholder="Username"
+                              required=""
+                            /><i class="bi bi-envelope-fill  text-center "></i>
+                          </div>
+                          <div class="form pt-4">
+                            <input
+                              id="password"
+                              name="password"
+                              type="password"
+                              autocomplete="current-password"
+                              v-model="user.password"
+                              class="input"
+                              placeholder="Password"
+                              required=""
+                            /><i class="bi bi-lock-fill  text-center"></i>
+                          </div>
+              <div class="submit-container pb-4 pt-5 text-center">
                 <button type="submit" class="btn  btn-primary login-btn">
                   Login
 
@@ -54,9 +44,11 @@
                   ></span>
                 </button>
               </div>
-            </div>
-          </div>
         </div>
+        <div class="col-8 col-sm-12">
+                <!-- <i class="bi bi-envelope-fill  text-center "></i> -->
+        </div>
+       
         
       </div>
     </form>
@@ -106,61 +98,27 @@ function login(ev) {
 
 <style scoped>
  
- .login-container{
-    height:100vh;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-
- }
-.test {
-  margin: auto 0px;
+.logo-main{
+        height: 130px;
+        width: 200px;
 }
-.form-body {
-  padding: 20px 70px;
-}
-.submit-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.login-btn{
-  background:#759FE1;
-}
-.clsu-title {
-  text-shadow: 3px 3px #e0e0e0;
-}
-.card {
-  box-shadow: 25px 25px 10px -10px rgba(0, 0, 0, 0.3);
-}
-h1 {
-  color: #1f5f5b;
-}
-h3 {
-  color: #1f5f5b;
-}
-.img-logo {
-  width: 155px;
-  height: 155px;
-}
-.btn {
-  min-width: 100%;
-  min-height: 45px;
-}
-.forgot {
-  text-decoration: none;
-  display: flex;
-  justify-content: end;
-  padding: 0px 40px;
+    .input {
+ padding-left: 10px;
+  border-radius: 10px;
+  border: 2px solid #97989e;
+  width: 90%;
+  height: 3rem;
 }
 .input {
   text-align: left;
   padding-left: 40px;
-  outline: none;
-  border: none;
-  border-bottom: 2px solid #1f5f5b;
+  /* outline: none; */
+  /* border: none; */
+  border: 2px solid #97989e;
   width: 100%;
+
 }
+
 .form {
   position: relative;
 }
@@ -169,14 +127,35 @@ h3 {
   left: 1%;
   border-radius: 100%;
   top: 40%;
-  background-color: #1f5f5b;
   padding: 1px 5px 10px 5px;
+
+  z-index: 1;
 }
 .bi {
-  height: 27px;
-  width: 27px;
+  font-size: 25px;
+  color: #bdbec1;
+  z-index: 1;
 }
-.forgot-txt {
-  color: #1f5f5b;
+.welcome-txt{
+  color: #072f5f;
+
+}
+.login-btn{
+  width: 350px;
+  background-color: #1e88e5;
+  border: 2px #1e88e5;
+  height: 40px;
+}
+.login-btn:hover{
+  background-color: #1976d2;
+  border: 2px #1976d2;
+}
+.card-base{
+  margin-left: 50px;
+  margin-top:40px;
+}
+.card {
+  box-shadow: 25px 25px 10px -5px rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
 }
 </style>
