8735bb75205bc3d6d2f4a36247ef890ba0eb1d89
Fernando_Jose <lopez.fernando@clsu2.edu.ph>
2023-03-10 17:01:42 +0800

    sendEmailDesign

--- resources/views/eventNotification.blade.php
+++ resources/views/eventNotification.blade.php
@@ -1,1 +1,25 @@
-test email {{$name}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    test email {{$name}}
+    <div class="container justify-content-center d-flex">
+      <div class="card text-center" style="max-width: 40vw;">
+        <img src="../assets/img/undraw_mail_sent_re_0ofv.svg" class="card-img-top py-4" alt="emailSend">
+        <div class="card-body">
+          <h5 class="card-title">Event Registration</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id accusamus fuga quia minima expedita doloremque
+             esse itaque sunt eligendi dignissimos porro neque, praesentium placeat, ea non illum et veritatis iusto!</p>
+        </div>
+        <div class="card-footer text-muted">
+          <p>Tagumpay Nagaño High School</p>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
--- vue/src/views/Thankyou.vue
+++ vue/src/views/Thankyou.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="fluid-container">
     <!-- Button trigger modal -->
-    <button type="button" class="btn btn-success shadow" data-bs-toggle="modal" data-bs-target="#thanks">
+    <!-- <button type="button" class="btn btn-success shadow" data-bs-toggle="modal" data-bs-target="#thanks">
       <i class="bi bi-send-fill"></i> Send Email
-    </button>
+    </button> -->
 
     <!-- Modal Email-->
     <div class="modal fade" id="thanks" tabindex="-1" aria-labelledby="thanks" aria-hidden="true">
@@ -24,20 +24,24 @@
         </div>
       </div>
     </div>
-    <!-- <div class="card-box">
-        <div class="card text-center mb-3 shadow">
-            <div class="p-1 mt-3">
-                <img src="../assets/img/undraw_mail_sent_re_0ofv.svg" class="card-img-top" alt="emailSend">
-            </div>
-            <div class="card-body">
-                <h3 class="card-title fw-bold">Thank You For Your Registration</h3>
-                <p class="card-text mt-4 text-black-50">We have send you an confirmation email to "yourname@email.com" <br>
-                Please confirm your email address thank you so much.</p>
-                <a href="#" class="btn btn-primary mt-3">Continue</a>
-            </div>
+
+    <div class="container justify-content-center d-flex">
+      <div class="card text-center" style="max-width: 40vw;">
+        <img src="../assets/img/undraw_mail_sent_re_0ofv.svg" class="card-img-top py-4" alt="emailSend">
+        <div class="card-body">
+          <h5 class="card-title">Event Registration</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id accusamus fuga quia minima expedita doloremque
+             esse itaque sunt eligendi dignissimos porro neque, praesentium placeat, ea non illum et veritatis iusto!</p>
         </div>
-    </div> -->
+        <div class="card-footer text-muted">
+          <p>Tagumpay Nagaño High School</p>
+        </div>
+    </div>
   </div>
+
+
+  </div>
+  
 </template>
 
 <script>
