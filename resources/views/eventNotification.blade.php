fe981e9857bcd1cb18e16543d277fa8f6bfabeda
Fernando_Jose <lopez.fernando@clsu2.edu.ph>
2023-03-11 10:17:33 +0800

    send

--- resources/views/eventNotification.blade.php
+++ resources/views/eventNotification.blade.php
@@ -7,19 +7,25 @@
     <title>Document</title>
 </head>
 <body>
-    test email {{$name}}
-    <div class="container justify-content-center d-flex">
-      <div class="card text-center" style="max-width: 40vw;">
-        <img src="https://i.pinimg.com/originals/af/8d/9a/af8d9ac62dd43026957e1cf82a027637.gif" class="card-img-top py-4" alt="emailSend">
-        <div class="card-body">
-          <h5 class="card-title">Event Registration</h5>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id accusamus fuga quia minima expedita doloremque
-             esse itaque sunt eligendi dignissimos porro neque, praesentium placeat, ea non illum et veritatis iusto!</p>
-        </div>
-        <div class="card-footer text-muted">
-          <p>Tagumpay Nagaño High School</p>
-        </div>
+    <!-- test email {{$name}} -->
+    <div class="fluid-container" style="padding: 5% 20% 10px;">>
+        <div class="card-box">
+          <div class="img-card" align="center" style="width: 100%;">
+            <img  src="https://img.freepik.com/premium-vector/teamwork-business-collection_171919-1245.jpg?w=826" alt="emailSend" style="height: 300px;">
+          </div>
+          <div class="card-body-container" style="width: 90%; padding: 10px;">
+            <h1 class="card-title-name" align="center">Event Registration Confirmation</h1>
+            <br>
+            <p class="card-text-content">Hi, {{$name}}</p>
+            <p>Thank you for your time to register, Hope you enjoy the event, Thank you!</p>
+            <br>
+            <p>If you have any concerns, please let us know at tnhs@gmail.com</p>
+            <p class="sincerely" align="right">Kind Regards,</p>
+          </div>
+          <div class="card-footer-name" align="center" style="background: #2269a7; color: #ffff; border-radius: 3px; padding: 10px;">
+            <small>Tagumpay Nagaño High School</small>
+          </div>
+      </div>
     </div>
-  </div>
 </body>
 </html>
